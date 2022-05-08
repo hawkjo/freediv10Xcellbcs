456337ace322066ef7912699d71358e9b141f043
John Hawkins <hawkjo@gmail.com>
2022-05-08 12:20:55 +0200

    Bugfix iterate possible main commands

--- freediv10Xcellbcs/config.py
+++ freediv10Xcellbcs/config.py
@@ -18,7 +18,7 @@ class CommandLineArguments(object):
     @property
     def command(self):
         # We have to do this weird loop to deal with the way docopt stores the command name
-        for possible_command in ('decode'):
+        for possible_command in ('decode',):
             if self._arguments.get(possible_command):
                 return possible_command
 
